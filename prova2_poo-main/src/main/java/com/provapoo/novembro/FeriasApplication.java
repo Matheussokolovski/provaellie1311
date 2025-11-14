46870cd148f17a0c5d89d5068131aa1ae96af262
Matheussokolovski <matheussokolovski@gmail.com>
2025-11-13 21:49:46 -0300

    Add files via upload

---
+++ prova2_poo-main/src/main/java/com/provapoo/novembro/FeriasApplication.java
@@ -0,0 +1,12 @@
+package com.provapoo.novembro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeriasApplication.class, args);
+	}
+}
